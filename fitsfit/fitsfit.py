99794a82b8b3adef39d9b4ec261ba3e2cd176e15
RHy910 <Rhyacinthe@colgate.edu>
2024-09-28 17:08:06 -0400

    forms needs some stuff

--- fitsfit/fitsfit.py
+++ fitsfit/fitsfit.py
@@ -1,9 +1,17 @@
-from flask import Flask, render_template
-app = Flask(__name__)
+from flask import Flask
+from flask import render_template
+from config import Config
+from routes import main as main_routes
+from models import db
 
-@app.route("/")
-def home():
-    return render_template('home.html')
+
+app = Flask(__name__)
+app = Flask(__name__)
+app.config.from_object(Config)
+
+db.init_app(app)
+
+app.register_blueprint(main_routes)
 
 if __name__ == '__main__':
     app.run(debug=True)
